14be6a7ec2a572f0303dadf087205725d9589d32
rodriman <mrodriguezgomez@iesesteveterradas.cat>
2016-01-29 17:23:11 +0100

    Actualización de los archivos
    Actualización de los archivos

--- 1.php
+++ 1.php
@@ -7,7 +7,9 @@
 
 <form method="POST" action="procesa_datos.php"> 
     <p>Nombre: <input type="text" name="T1" size="20"></p> 
-    <p>Apellidos: <input type="text" name="T2" size="20"></p> 
+    <p>Apellidos: <input type="text" name="T2" size="20"></p>
+	<p>Edad: <input type="text" name="T2" size="20"></p>
+	<p>DNI: <input type="text" name="T2" size="20"></p>
     <p><input type="submit" value="Enviar" name="B1"></p> 
 </form> 
 
@@ -24,8 +26,9 @@
 <?php 
 $nombre = $_POST['T1']; 
 $apellidos = $_POST['T2']; 
-
-echo "$nombre $apellidos"; 
+$edad = $_POST['T2']
+$dni = $_POST['T2']
+echo "$nombre $apellidos $edad $dni"; 
 ?> 
 
 </body> 
--- 2.js
+++ 2.js
@@ -1,11 +1,14 @@
-﻿<html> 
-<head> 
-<title>Mensaje al pasar el raton sobre un enlace</title> 
-</head> 
-<body> 
-Pasa el ratón por encima del enlace  
-<a href="https://www.bancsabadell.es"  
-ONMOUSEOVER="javascript:alert('Estas en BANCSABADELL.ES ¿que tal estas?');return false" 
->BANCSABADELL.ES</a> y podrás ver un mensaje. 
-</body> 
-</html>
+﻿<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First JavaScript</h1>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+
+</body>
+</html> 
